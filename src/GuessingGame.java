a8303fb3fbabced9d181d91b95bcdec9b7dba799
MachariaKuria <skuria@student.cscc.edu>
2018-08-26 12:01:13 -0400

    initial logic

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
 
 public class GuessingGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 
+		Scanner input = new Scanner(System.in);
+		
+		int secretNumber = 5;
+		
+		System.out.println("Welcome to our guessing game!");
+		System.out.println("Choose a number between 1 and 10: ");
+		int guessedNumber = input.nextInt();
+		
+		if(secretNumber == guessedNumber) {
+			System.out.println("You guessed it!");
+		}
+		else {
+			System.out.println("Sorry you LOSE!!!");
+		}
+		
 	}
 
 }
